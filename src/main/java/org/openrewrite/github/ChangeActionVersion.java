e33fd8a7cf5b2ad3754491a0888d60a9b12afbe6
Tim te Beek <tim@moderne.io>
2023-12-02 16:58:20 +0100

    Change the version of a GitHub Action
    Fixes #23

---
+++ src/main/java/org/openrewrite/github/ChangeActionVersion.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.github;
+
+import lombok.RequiredArgsConstructor;
+import org.openrewrite.*;
+import org.openrewrite.yaml.ChangeValue;
+
+@RequiredArgsConstructor
+public class ChangeActionVersion extends Recipe {
+    @Option(displayName = "Action",
+            description = "Name of the action to update.",
+            example = "actions/setup-java")
+    private final String action;
+
+    @Option(displayName = "Version",
+            description = "Version to use.",
+            example = "v4")
+    private final String version;
+
+    @Override
+    public String getDisplayName() {
+        return "Change GitHub Action version";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Change the version of a GitHub Action in any `.github/workflows/*.yml` file.";
+    }
+
+    @Override
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return Preconditions.check(
+                new HasSourcePath(".github/workflows/*.yml"),
+                new ChangeValue(
+                        "$.jobs..steps[?(@.uses =~ '" + action + "(?:@.+)?')].uses",
+                        action + '@' + version).getVisitor());
+    }
+}
