4d8639100ae84f4023e946697798c6834eef1c19
Tim te Beek <tim@moderne.io>
2024-04-15 14:17:41 +0200

    Rename GitHub Actions for Gradle (#102)
    Fixes https://github.com/openrewrite/rewrite-github-actions/issues/101

---
+++ src/test/java/org/openrewrite/github/ChangeActionTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.github;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.yaml.Assertions.yaml;
+
+class ChangeActionTest implements RewriteTest {
+    @DocumentExample
+    @Test
+    void updateActionVersion() {
+        rewriteRun(
+          spec -> spec.recipe(new ChangeAction(
+            "gradle/wrapper-validation-action",
+            "gradle/actions/wrapper-validation",
+            "v3")),
+          //language=yaml
+          yaml(
+            """
+              jobs:
+                deploy:
+                  runs-on: ubuntu-latest
+                  steps:
+                    - name: Checkout
+                      uses: actions/checkout@v4
+                    - name: Validate wrapper
+                      uses: gradle/wrapper-validation-action@v2
+              """,
+            """
+              jobs:
+                deploy:
+                  runs-on: ubuntu-latest
+                  steps:
+                    - name: Checkout
+                      uses: actions/checkout@v4
+                    - name: Validate wrapper
+                      uses: gradle/actions/wrapper-validation@v3
+              """,
+            source -> source.path(".github/workflows/ci.yml")
+          )
+        );
+    }
+
+    @Test
+    void setupGradleYamlRecipe() {
+        rewriteRun(
+          spec -> spec.recipeFromResources("org.openrewrite.github.gradle.RenameGradleBuildActionToSetupGradle"),
+          //language=yaml
+          yaml(
+            """
+              jobs:
+                deploy:
+                  runs-on: ubuntu-latest
+                  steps:
+                    - name: Checkout
+                      uses: actions/checkout@v4
+                    - uses: gradle/gradle-build-action@v3
+              """,
+            """
+              jobs:
+                deploy:
+                  runs-on: ubuntu-latest
+                  steps:
+                    - name: Checkout
+                      uses: actions/checkout@v4
+                    - uses: gradle/actions/setup-gradle@v3
+              """,
+            source -> source.path(".github/workflows/ci.yml")
+          )
+        );
+    }
+}
