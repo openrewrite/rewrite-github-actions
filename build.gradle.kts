cd771615d36cdc79a8a61b5b462df82672d9ec69
Tim te Beek <tim@moderne.io>
2025-01-16 22:43:33 +0100

    Apply MSAL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.openrewrite.build.recipe-library") version "latest.release"
-    id("org.openrewrite.build.moderne-proprietary-license") version "latest.release"
+    id("org.openrewrite.build.moderne-source-available-license") version "latest.release"
 }
 
 group = "org.openrewrite.recipe"
