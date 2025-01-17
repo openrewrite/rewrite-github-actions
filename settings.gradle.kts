7775b6d9ebc9fea37eb6abc940f18772b40639ce
Tim te Beek <tim@moderne.io>
2025-01-17 11:52:31 +0100

    Add Gradle enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,40 @@
 rootProject.name = "rewrite-github-actions"
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("com.gradle.develocity") version "latest.release"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "latest.release"
+}
+
+develocity {
+    server = "https://ge.openrewrite.org/"
+    val isCiServer = System.getenv("CI")?.equals("true") ?: false
+    val accessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY")
+    val authenticated = !accessKey.isNullOrBlank()
+    buildCache {
+        remote(develocity.buildCache) {
+            isEnabled = true
+            isPush = isCiServer && authenticated
+        }
+    }
+
+    buildScan {
+        capture {
+            fileFingerprints = true
+        }
+
+        publishing {
+            onlyIf {
+                authenticated
+            }
+        }
+
+        uploadInBackground = !isCiServer
+    }
+}
