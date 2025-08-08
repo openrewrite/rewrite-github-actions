142cdf1ca76c4f059372b5e88610e47fa55e7906
Greg Oledzki <greg.oledzki@moderne.io>
2025-08-08 15:18:54 +0200

    Import order

--- src/test/java/org/openrewrite/github/RemoveWorkflowInputArgumentTest.java
+++ src/test/java/org/openrewrite/github/RemoveWorkflowInputArgumentTest.java
@@ -15,11 +15,11 @@
  */
 package org.openrewrite.github;
 
-import org.junit.jupiter.api.Test;
-
 import org.openrewrite.DocumentExample;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
+
+import org.junit.jupiter.api.Test;
 
 import static org.openrewrite.yaml.Assertions.yaml;
 
