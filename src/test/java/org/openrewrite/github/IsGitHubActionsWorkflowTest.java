9ccb71ea8672503998b543d94b3e8a7c32d5e2ba
Tim te Beek <tim@moderne.io>
2025-11-10 18:47:06 +0100

    Test precondition separately

---
+++ src/test/java/org/openrewrite/github/IsGitHubActionsWorkflowTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Moderne Source Available License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://docs.moderne.io/licensing/moderne-source-available-license
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.github;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.marker.SearchResult;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openrewrite.yaml.Assertions.yaml;
+
+class IsGitHubActionsWorkflowTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new IsGitHubActionsWorkflow());
+    }
+
+    @DocumentExample
+    @Test
+    void detectYml() {
+        rewriteRun(
+          //language=yml
+          yaml(
+            """
+              on:
+                push:
+                  branches:
+                    - main
+              """,
+            """
+              on:
+                push:
+                  branches:
+                    - main
+              """,
+            spec -> spec.path(".github/workflows/ci.yml")
+              .afterRecipe(docs -> assertThat(docs.getMarkers().findFirst(SearchResult.class)).isPresent())
+          )
+        );
+    }
+
+    @Test
+    void detectYaml() {
+        rewriteRun(
+          //language=yml
+          yaml(
+            """
+              on:
+                push:
+                  branches:
+                    - main
+              """,
+            """
+              on:
+                push:
+                  branches:
+                    - main
+              """,
+            spec -> spec.path(".github/workflows/ci.yaml")
+              .afterRecipe(docs -> assertThat(docs.getMarkers().findFirst(SearchResult.class)).isPresent())
+          )
+        );
+    }
+
+    @Nested
+    class NotFound {
+        @Test
+        void wrongFolder() {
+            rewriteRun(
+              //language=yml
+              yaml(
+                """
+                  on:
+                    push:
+                      branches:
+                        - main
+                  """,
+                spec -> spec.path(".github/workflow/ci.yml")
+                  .afterRecipe(docs -> assertThat(docs.getMarkers().findFirst(SearchResult.class)).isEmpty())
+              )
+            );
+        }
+
+        @Test
+        void wrongExtension() {
+            rewriteRun(
+              //language=yml
+              yaml(
+                """
+                  on:
+                    push:
+                      branches:
+                        - main
+                  """,
+                spec -> spec.path(".github/workflows/ci.yaaml")
+                  .afterRecipe(docs -> assertThat(docs.getMarkers().findFirst(SearchResult.class)).isEmpty())
+              )
+            );
+        }
+    }
+}
