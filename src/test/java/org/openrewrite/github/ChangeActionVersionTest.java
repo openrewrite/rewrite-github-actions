3a10901ab03a0a5cd1bf582a44fc5fb533123685
Willem Jan Glerum <willem.jan.glerum@lunatech.nl>
2025-12-16 23:52:30 +0100

    Allow updating multiple actions using a wildcard pattern (#164)
    * Allow to update nested actions
    
    * `ChangeValue` does not support wildcards in replacement pattern
    
    * Address code suggestions
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>

--- src/test/java/org/openrewrite/github/ChangeActionVersionTest.java
+++ src/test/java/org/openrewrite/github/ChangeActionVersionTest.java
@@ -17,6 +17,7 @@ package org.openrewrite.github;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.DocumentExample;
+import org.openrewrite.Issue;
 import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.yaml.Assertions.yaml;
@@ -52,6 +53,7 @@ class ChangeActionVersionTest implements RewriteTest {
 
     @Test
     void updateActionVersionYaml() {
+        //language=yaml
         rewriteRun(
           spec -> spec.recipeFromYaml("""
             type: specs.openrewrite.org/v1beta/recipe
@@ -63,7 +65,6 @@ class ChangeActionVersionTest implements RewriteTest {
                   action: actions/setup-java
                   version: v4
             """, "org.example.SetupJavaV4"),
-          //language=yaml
           yaml(
             """
               jobs:
@@ -82,6 +83,36 @@ class ChangeActionVersionTest implements RewriteTest {
                     - uses: actions/setup-java@v4
               """,
             source -> source.path(".github/workflows/ci.yml")
+          )
+        );
+    }
+
+    @Issue("https://github.com/openrewrite/rewrite-github-actions/issues/163")
+    @Test
+    void updateActionVersionWithWildcard() {
+        rewriteRun(
+          spec -> spec.recipe(new ChangeActionVersion("actions/nested.*", "v4")),
+          //language=yaml
+          yaml(
+            """
+              jobs:
+                build:
+                  runs-on: ubuntu-latest
+                  steps:
+                    - uses: actions/checkout@v2
+                    - uses: actions/nested/checkout@v2
+                    - uses: actions/nested/setup-java@main
+              """,
+            """
+              jobs:
+                build:
+                  runs-on: ubuntu-latest
+                  steps:
+                    - uses: actions/checkout@v2
+                    - uses: actions/nested/checkout@v4
+                    - uses: actions/nested/setup-java@v4
+              """,
+            source -> source.path(".github/workflows/ci.yaml")
           )
         );
     }
